fc5f5054abb9f743c4dd74832affb62ebdc04d32
Enkhe Garmaev <garmaevenhe@gmail.com>
2025-03-13 23:47:08 +0900

    feat: add burger menu

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function handleFormSubmit(event) {
-  // Do not submit these forms yourself.
+  // do not submit these forms yourself.
   event.preventDefault();
   console.log('Отправка формы');
-  // We receive form data
+  // receive form data
   const formData = new FormData(event.target);
 
-  // We output each field to the console
+  // output each field to the console
   for (let [key, value] of formData.entries()) {
     console.log(`${key}: ${value}`);
   }
@@ -13,3 +13,22 @@ function handleFormSubmit(event) {
 
 const applicantForm = document.getElementById('form-once');
 applicantForm.addEventListener('submit', handleFormSubmit);
+
+//menu transform
+function myFunction(x) {
+  x.classList.toggle('change');
+}
+
+function transform() {
+  let elements = document.getElementsByClassName('link');
+
+  for (let i = 0; i < elements.length; i++) {
+    let element = elements[i];
+
+    if (element.style.display === 'inline') {
+      element.style.display = 'none';
+    } else {
+      element.style.display = 'inline';
+    }
+  }
+}
